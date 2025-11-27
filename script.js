897bebb4f89e5b8ce4ed2972788e10e327082f96
Chatbotsinaction <padvodnayalotka1@hotmail.com>
2025-11-27 11:27:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -166,7 +166,7 @@ function appendQA(questionText, answerText) {
             output.innerHTML = "";
             userInput.textContent = "";
             qaCount = 0;
-        }, 10000);
+        }, 8000);
     }
 }
 
